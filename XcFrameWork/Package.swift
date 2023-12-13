206535208367034df5b185a90a0cbe90ed387d04
Anand Bhavsar <anand.bhavsar@spec-india.com>
2023-12-13 16:55:46 +0530

    add zip

--- XcFrameWork/Package.swift
+++ XcFrameWork/Package.swift
@@ -27,6 +27,26 @@
 
 import PackageDescription
 
+//let package = Package(
+//    name: "XcFrameWork",
+//    platforms: [
+//        .iOS(.v12),
+//        .tvOS(.v12)
+//    ],
+//    products: [
+//        .library(
+//            name: "XcFrameWork",
+//            targets: ["XcFrameWork"]
+//        ),
+//    ],
+//    targets: [
+//        .binaryTarget(
+//            name: "XcFrameWork",
+//            url: "https://github.com/abhasar1008/SwiftPackageManagerXcframeWork/blob/main/XcFrameWork/ConstanantKey.zip",
+//            checksum: "d332c0133c02bb18e680f0c6ceb1e6ede6d9c75274b59dba18b50f29a9bc7c0c"
+//        ),
+//    ]
+//)
 let package = Package(
     name: "XcFrameWork",
     platforms: [
@@ -36,14 +56,21 @@ let package = Package(
     products: [
         .library(
             name: "XcFrameWork",
-            targets: ["XcFrameWork"]
+            targets: ["XcFrameWork","XcFrameWork1"]
         ),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "XcFrameWork"),
+        .testTarget(
+            name: "XcFrameWorkTests",
+            dependencies: ["XcFrameWork"]),
         .binaryTarget(
-            name: "XcFrameWork",
-            url: "https://github.com/abhasar1008/SwiftPackageManagerXcframeWork/blob/main/XcFrameWork/TestingFramework.xcframework.zip",
-            checksum: "c816c6f98038ba954949c07733e1e0bd9d429e47a3d336d2af89520834a3bf4e"
+            name: "XcFrameWork1",
+            path: "Sources/ConstanantKey.xcframework"
         ),
     ]
 )
+
