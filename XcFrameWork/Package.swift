2f846101bf2416568c86ca8b0b6323dfcba3ac50
Anand Bhavsar <anand.bhavsar@spec-india.com>
2023-12-13 14:44:32 +0530

    add zip

--- XcFrameWork/Package.swift
+++ XcFrameWork/Package.swift
@@ -1,26 +1,49 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+//import PackageDescription
+//import Foundation
+//
+//let package = Package(
+//    name: "XcFrameWork",
+//    platforms: [
+//           .iOS(.v12),
+//           .tvOS(.v12)
+//       ],
+//    products: [
+//        // Products define the executables and libraries a package produces, making them visible to other packages.
+//        .library(
+//            name: "XcFrameWork",
+//            targets: ["XcFrameWork"]),
+//    ],
+//    targets: [
+//        .binaryTarget(name:"TestingFramework", url: "https://github.com/abhasar1008/SwiftPackageManagerXcframeWork/blob/main/XcFrameWork/TestingFramework.zip", checksum:"c816c6f98038ba954949c07733e1e0bd9d429e47a3d336d2af89520834a3bf4e"),
+//        .testTarget(
+//            name: "XcFrameWork",
+//            dependencies: ["XcFrameWork"])
+//    ]
+//)
+
+
 import PackageDescription
-import Foundation
 
 let package = Package(
     name: "XcFrameWork",
+    platforms: [
+        .iOS(.v11),
+        .tvOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "XcFrameWork",
-            targets: ["XcFrameWork"]),
+            targets: ["XcFrameWork"]
+        ),
     ],
     targets: [
-        .binaryTarget(name:"TestingFramework", url: "https://github.com/abhasar1008/SwiftPackageManagerXcframeWork/blob/main/XcFrameWork/TestingFramework.zip", checksum:"c816c6f98038ba954949c07733e1e0bd9d429e47a3d336d2af89520834a3bf4e"),
-        .target(
-            name: "XcFrameWork"),
-        .testTarget(
-            name: "XcFrameWorkTests",
-            dependencies: ["XcFrameWork"]),
+        .binaryTarget(
+            name: "XcFrameWork",
+            url: "https://bitbucket.org/usercentricscode/usercentrics-spm-sdk/downloads/Usercentrics-2.10.0.xcframework.zip",
+            checksum: "240bc1dea39a342ee272cbc61fa5629ae860012c218dca113d99296d914c844a"
+        ),
     ]
 )
-
-
-
